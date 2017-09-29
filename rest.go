058b3b63051579f4daa6fff21ea09bf870636079
Borje Granberg <borje.granberg@gmail.com>
2017-09-30 00:28:38 +0200

    Update viper and pwm with target temp. Enable to read out config values via REST

--- rest.go
+++ rest.go
@@ -3,19 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 
-	"github.com/gorilla/mux"
-	"github.com/unrolled/render"
-	//"github.com/unrolled/render"
 	"github.com/felixge/pidctrl"
+	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
+	"github.com/unrolled/render"
 )
 
-func initRest(p *Pwm, pid *pidctrl.PIDController) {
-	r := render.New(render.Options{})
+func initRest(pwm *Pwm, pid *pidctrl.PIDController) {
+	r := render.New(render.Options{IndentJSON: true})
 	router := mux.NewRouter()
 
 	router.HandleFunc("/dutycycle", func(w http.ResponseWriter, req *http.Request) {
-		dutycyclePercentage := p.GetDutyCycle() * 100.0
+		dutycyclePercentage := pwm.GetDutyCycle() * 100.0
 		log.Println("/dutycycle was delivered ", dutycyclePercentage)
 		r.JSON(w, http.StatusOK, dutycyclePercentage)
 	})
@@ -23,7 +24,23 @@ func initRest(p *Pwm, pid *pidctrl.PIDController) {
 	router.HandleFunc("/target/{value}", func(w http.ResponseWriter, req *http.Request) {
 		vars := mux.Vars(req)
 
-		log.Println("received REST call to set Target temp to", vars["value"])
+		if f, err := strconv.ParseFloat(vars["value"], 64); err == nil {
+			log.Println("received REST call to set Target temp to", f)
+			viper.Set("pid.target", f)
+			pid.Set(f)
+		} else {
+			log.Println("Failed to parse given Target temp as float", vars["value"])
+		}
+	})
+
+	router.HandleFunc("/config", func(w http.ResponseWriter, req *http.Request) {
+		p, i, d := pid.PID()
+		values := map[string]float64{}
+		values["p"], values["i"], values["d"] = p, i, d
+		values["target"] = viper.GetFloat64("pid.target")
+		values["dutycycle"] = pwm.GetDutyCycle()
+		r.JSON(w, http.StatusOK, values)
+		log.Println(p, i, d)
 	})
 
 	http.ListenAndServe(":8081", router)
